f6184fc318d81a4f02446a946edb3bf8060fe7e8
Victor Sydorov <Victor.sydorov@gmail.com>
2023-08-06 21:35:41 +0300

    UC#10 Implement regex tests

--- Use_case_10/StringValidator.cs
+++ Use_case_10/StringValidator.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Use_case_10
 {
-    public  class StringValidator
+    public class StringValidator
     {
         private readonly int min;
         private readonly int max;
-        private readonly string predefinedChars = @"!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~.";
 
         public StringValidator(int min, int max)
         {
@@ -20,21 +13,30 @@ namespace Use_case_10
             this.min = min;
             this.max = max;
         }
-        public  bool Validate(string str) {
+        public bool Validate(string str)
+        {
+            if (str == null) {  throw new ArgumentNullException("str"); }
 
-            bool match = false;
-            match &= Regex.IsMatch(str, $"[.]{{{min},{max}}}");
+            bool match = true;
+
+            match &= Regex.IsMatch(str, $"^.{{{min},{max}}}$");
             if (!match) { return match; }
+
             match &= Regex.IsMatch(str, $"[a-z]+");
             if (!match) { return match; }
+
             match &= Regex.IsMatch(str, $"[A-Z]+");
             if (!match) { return match; }
+
             match &= Regex.IsMatch(str, $"\\d+");
             if (!match) { return match; }
+
             match &= Regex.IsMatch(str, $"[!\"#$%&'()*+,-./:;<=>?@[\\]^_`{{|}}~.]+");
             if (!match) { return match; }
+
             match &= Regex.IsMatch(str, $"^\\S*$");
-            return match;                    
+
+            return match;
         }
     }
 }
---
+++ Use_case_10Tests/StringValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Use_case_10.Tests
+{
+    public class StringValidatorTests
+    {
+        [Theory()]
+        [InlineData("aA1", "Failed min length check")]
+        [InlineData("123456Aa+++", "Failed max length check")]
+        [InlineData("aa1+aa", "Failed a capital letter  check")]
+        [InlineData("AA1+AA", "Failed a lower letter  check")]
+        [InlineData("aaA+aa", "Failed a digit check")]
+        [InlineData("111aAaa", "Failed  a special char check")]
+        [InlineData("aaaAA+A+", "Failed  a digit check")]
+        [InlineData("aaa A+A+", "Failed no whitespace check")]
+        [InlineData("a A+a A+", "Failed no whitespace check")]
+        [InlineData("a a A+A+", "Failed no whitespace check")]
+        public void ValidateTest_Invalid_Input_Returns_False(string str, string error)
+        {
+            // act
+            bool result = new StringValidator(4,8).Validate(str);
+
+            // assert
+            Assert.False(result, error);
+        }
+
+        [Theory()]
+        [InlineData("aA1+")]
+        [InlineData("aA1+aaa")]
+        [InlineData("aA1+1")]
+        public void ValidateTest_Valid_Input_Returns_True(string str)
+        {
+            // act
+            bool result = new StringValidator(4, 8).Validate(str);
+
+            // assert
+            Assert.True(result);
+        }
+    }
+}

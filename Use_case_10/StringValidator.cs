00d8cca22a05cf6575c0577091a1fa9c3220c8e5
Victor Sydorov <Victor.sydorov@gmail.com>
2023-08-06 21:00:15 +0300

    UC#10 Implement regex

---
+++ Use_case_10/StringValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Use_case_10
+{
+    public  class StringValidator
+    {
+        private readonly int min;
+        private readonly int max;
+        private readonly string predefinedChars = @"!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~.";
+
+        public StringValidator(int min, int max)
+        {
+            if (min > max) { throw new ArgumentException("Min value should be less or equal max value"); }
+            this.min = min;
+            this.max = max;
+        }
+        public  bool Validate(string str) {
+
+            bool match = false;
+            match &= Regex.IsMatch(str, $"[.]{{{min},{max}}}");
+            if (!match) { return match; }
+            match &= Regex.IsMatch(str, $"[a-z]+");
+            if (!match) { return match; }
+            match &= Regex.IsMatch(str, $"[A-Z]+");
+            if (!match) { return match; }
+            match &= Regex.IsMatch(str, $"\\d+");
+            if (!match) { return match; }
+            match &= Regex.IsMatch(str, $"[!\"#$%&'()*+,-./:;<=>?@[\\]^_`{{|}}~.]+");
+            if (!match) { return match; }
+            match &= Regex.IsMatch(str, $"^\\S*$");
+            return match;                    
+        }
+    }
+}
